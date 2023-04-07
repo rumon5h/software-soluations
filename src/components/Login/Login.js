3523d4ce3abaad0975bc0f0352eaf9ae06202de9
rumon5h <mdrumonkhan48519@gmail.com>
2023-04-07 20:30:05 +0600

    done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { toast } from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        
+        const user = {
+            email: event.target.email.value,
+            password: event.target.password.value
+        }
+        console.log(user);
+        toast.success('Successfully submitted the information.')
+    }
     return (
 
         <div
             style={{ backgroundImage: `url('/images/provide-background.webp')` }}
             className="hero min-h-screen image-full bg-base-200">
 
-            <div className="card flex-shrink-0 py-5 w-full max-w-lg shadow-2xl bg-base-100">
+            <form 
+            onSubmit={handleSubmit}
+            className="card flex-shrink-0 py-5 w-full max-w-lg shadow-2xl bg-base-100">
                 <h1 className='text-3xl text-center'>Login Now</h1>
 
                 <div className="card-body">
@@ -16,13 +29,13 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input name='email' type="email" placeholder="email" className="input input-bordered" />
+                        <input required name='email' type="email" placeholder="email" className="input input-bordered" />
                     </div>
                     <div className="form-control mt-3">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input name='password' type="password" placeholder="password" className="input input-bordered" />
+                        <input required name='password' type="password" placeholder="password" className="input input-bordered" />
                         <label className="label">
                             <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
                         </label>
@@ -31,10 +44,10 @@ const Login = () => {
                         </label>
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn">Login</button>
+                        <button type='submit' className="btn">Login</button>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
 
 
